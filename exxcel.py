3278eb2ca44702ea14038e364799652abeecd9eb
kaliani <kalyanivan@gmail.com>
2019-12-21 12:55:57 +0200

    ver.02

--- exxcel.py
+++ exxcel.py
@@ -1,17 +1,27 @@
+import click
 import sys
 import pandas as pd
 import clipboard
 
-filename = str(sys.argv[1])
 
-columnname = str(sys.argv[2])
+@click.command()
+@click.argument('filename')
+@click.argument('columnname')
 
-df = pd.read_excel(filename)
-string = ''
-for i in df.index:
-    if len(df.index)-1 > i:
-        string += "'"+str(df[columnname][i])+"'"+","+'\n'
-    else:
-        string += "'"+str(df[columnname][i])+"'"
 
-clipboard.copy(string)
+def main(filename, columnname):
+
+    df = pd.read_excel(filename)
+    string = ''
+
+    for i in df.index:
+        if len(df.index)-1 > i:
+            string += "'"+str(df[columnname][i])+"'"+","+'\n'
+        else:
+            string += "'"+str(df[columnname][i])+"'"
+
+    clipboard.copy(string)
+
+
+if __name__ == "__main__":
+    main()
